4d5a95e6671524e2c477102140f246dbb460a560
jaytwitch <johnathan.tiong@gmail.com>
2020-08-02 12:51:32 +1000

    updated keystone main menu in shell script
    Signed-off-by: jaytwitch <johnathan.tiong@gmail.com>

--- start.sh
+++ start.sh
@@ -102,130 +102,144 @@ show_menu() {
 #
 # OUTPUT/INTERFACE
 #
-clear
-show_menu main
+while true; do
 
-# take input
-read -p "Select an option: " option
-until [[ "$option" =~ ^[0-8]$ ]]; do
-    echo -e "$option: invalid selection."
+    clear
+    show_menu main
+
+    # take input
     read -p "Select an option: " option
+    until [[ "$option" =~ ^[0-8]$ ]]; do
+        echo -e "$option: invalid selection."
+        read -p "Select an option: " option
+    done
+
+    case "$option" in
+        1)
+            # clearing .git so it doesn't cause issues
+            # rm -rf ./.git
+
+            echo
+            echo -e "[ ${LIGHTPURPLE}STARTING    ${NOCOLOR} ] Keystone Core Infrastructure"
+            echo
+
+            #
+            # docker network create
+            #
+            if [ ! "$(docker network ls | grep ${NET_NAME})" ]; then
+                echo -ne "[ ${GREEN}DEPLOYING   ${NOCOLOR} ] CORE: docker container network ${NET_NAME} ...\033[0K\r"
+                docker network create -d bridge --subnet 172.25.0.0/16 --gateway 172.25.0.1 --ip-range= ${NET_NAME} > /dev/null 2>&1
+                echo -ne "[ ${LIGHTGREEN}SUCCESS     ${NOCOLOR} ] CORE: docker network ${NET_NAME} successfully created, continuing..."
+            else
+                echo -e "[ ${LIGHTRED}EXISTS      ${NOCOLOR} ] ${NET_NAME} docker network already exists, skipping..."
+            fi
+            echo
+
+            #
+            # launch:
+            #   nginx-web
+            #   nginx-letsencrypt
+            #   nginx-gen
+            #
+            echo -ne "[ ${GREEN}DEPLOYING   ${NOCOLOR} ] CORE: proxy containers ...\033[0K\r"
+            echo -ne "[ ${GREEN}CONFIGURING ${NOCOLOR} ] CORE: proxy network ...\033[0K\r"
+            printf "\nNETWORK=${NET_NAME}\n" >> ./core/proxy/.env
+            echo -ne "[ ${LIGHTGREEN}SUCCESS     ${NOCOLOR} ] CORE: proxy network configured\033[0K\r"
+            echo -ne "[ ${GREEN}STARTING    ${NOCOLOR} ] CORE: proxy containers ...\033[0K\r"
+            cd core/proxy && docker-compose up -d > /dev/null 2>&1
+            echo -ne "[ ${LIGHTGREEN}SUCCESS     ${NOCOLOR} ] CORE: proxy containers started\033[0K\r"
+            cd ../..
+            echo
+
+            #
+            # launch:
+            #    db-core
+            #    db-admin
+            #
+            echo -ne "[ ${GREEN}DEPLOYING   ${NOCOLOR} ] CORE: database containers ...\033[0K\r"
+            cd core/database
+
+            # create folders
+            mkdir -p data/logs
+            mkdir -p data/db
+            mkdir -p data/pma/sessions
+
+            echo -ne "[ ${GREEN}CONFIGURING ${NOCOLOR} ] CORE: db core container ...\033[0K\r"
+            printf "\nMYSQL_PASSWORD=${SQL_PASS}\n" >> .env
+            printf "\nNET_NAME=${NET_NAME}\n" >> .env
+            echo -ne "[ ${LIGHTGREEN}SUCCESS     ${NOCOLOR} ] CORE: db core container configured\033[0K\r"
+
+            echo -ne "[ ${GREEN}STARTING    ${NOCOLOR} ] CORE: database containers ...\033[0K\r"
+            docker-compose up -d > /dev/null 2>&1
+            echo -ne "[ ${LIGHTGREEN}SUCCESS     ${NOCOLOR} ] CORE: database containers started\033[0K\r"
+
+            cd ../..
+            echo
+
+            #
+            # launch:
+            #   portainer
+            #
+            cd core/portainer
+            echo -ne "[ ${GREEN}DEPLOYING   ${NOCOLOR} ] CORE: portainer orchestration containers ...\033[0K\r"
+            echo -ne "[ ${GREEN}CONFIGURING ${NOCOLOR} ] CORE: portainer orchestration container ...\033[0K\r"
+            printf "\nPORTAINER_URL=${PORTAINER_URL}\n" >> .env
+            printf "\nPORTAINER_PORT=${PORTAINER_PORT}\n" >> .env
+            printf "\nSSL_MAIL=${SSL_MAIL}\n" >> .env
+            printf "\nNET_NAME=${NET_NAME}\n" >> .env
+            echo -ne "[ ${LIGHTGREEN}SUCCESS     ${NOCOLOR} ] CORE: portainer orchestration container configured\033[0K\r"
+
+            echo -ne "[ ${GREEN}STARTING    ${NOCOLOR} ] CORE: portainer orchestration containers ...\033[0K\r"
+            docker-compose up -d > /dev/null 2>&1
+            echo -ne "[ ${LIGHTGREEN}SUCCESS     ${NOCOLOR} ] CORE: portainer orchestration containers started\033[0K\r"
+
+            cd ../..
+            # end of deployments
+
+            echo
+            echo
+            read -n 1 -s -r -p "Press any key to continue"
+            echo
+            ;;
+
+        2)
+            echo
+            echo -ne "[ ${GREEN}STARTING    ${NOCOLOR} ] CORE: all containers ...\033[0K\r"
+            cd core/portainer && docker-compose up -d && cd ../.. > /dev/null 2>&1
+            cd core/database && docker-compose up -d && cd ../.. > /dev/null 2>&1
+            cd core/proxy && docker-compose up -d && cd ../.. > /dev/null 2>&1
+            echo -ne "[ ${LIGHTRED}STARTED     ${NOCOLOR} ] CORE: all containers\033[0K\r"
+            echo
+            echo
+            read -n 1 -s -r -p "Press any key to continue"
+            echo
+            ;;
+
+        3)
+            echo
+            echo -ne "[ ${RED}STOPPING    ${NOCOLOR} ] CORE: all containers ...\033[0K\r"
+            cd core/portainer && docker-compose down > /dev/null 2>&1 && cd ../..
+            cd core/database && docker-compose down > /dev/null 2>&1 && cd ../..
+            cd core/proxy && docker-compose down > /dev/null 2>&1 && cd ../..
+            echo -ne "[ ${LIGHTRED}STOPPED     ${NOCOLOR} ] CORE: all containers\033[0K\r"
+            echo
+            echo
+            read -n 1 -s -r -p "Press any key to continue"
+            echo
+            ;;
+
+        5)
+            echo
+            echo -e "${WHITE}INITIATING DESTRUCT SEQUENCE...${NOCOLOR}"
+            destroy_all
+            ;;
+        0)
+            echo
+            echo -e "Goodbye!"
+            break;;
+    esac
+
 done
+exit
 
-case "$option" in
-    1)
-        # clearing .git so it doesn't cause issues
-        # rm -rf ./.git
-
-        echo
-        echo -e "[ ${LIGHTPURPLE}STARTING    ${NOCOLOR} ] Keystone Core Infrastructure"
-        echo
-
-        #
-        # docker network create
-        #
-        if [ ! "$(docker network ls | grep ${NET_NAME})" ]; then
-            echo -ne "[ ${GREEN}DEPLOYING   ${NOCOLOR} ] CORE: docker container network ${NET_NAME} ...\033[0K\r"
-            docker network create -d bridge --subnet 172.25.0.0/16 --gateway 172.25.0.1 --ip-range= ${NET_NAME} > /dev/null 2>&1
-            echo -ne "[ ${LIGHTGREEN}SUCCESS     ${NOCOLOR} ] CORE: docker network ${NET_NAME} successfully created, continuing..."
-        else
-            echo -e "[ ${LIGHTRED}EXISTS      ${NOCOLOR} ] ${NET_NAME} docker network already exists, skipping..."
-        fi
-        echo
-
-        #
-        # launch:
-        #   nginx-web
-        #   nginx-letsencrypt
-        #   nginx-gen
-        #
-        echo -ne "[ ${GREEN}DEPLOYING   ${NOCOLOR} ] CORE: proxy containers ...\033[0K\r"
-        echo -ne "[ ${GREEN}CONFIGURING ${NOCOLOR} ] CORE: proxy network ...\033[0K\r"
-        printf "\nNETWORK=${NET_NAME}\n" >> ./core/proxy/.env
-        echo -ne "[ ${LIGHTGREEN}SUCCESS     ${NOCOLOR} ] CORE: proxy network configured\033[0K\r"
-        echo -ne "[ ${GREEN}STARTING    ${NOCOLOR} ] CORE: proxy containers ...\033[0K\r"
-        cd core/proxy && docker-compose up -d > /dev/null 2>&1
-        echo -ne "[ ${LIGHTGREEN}SUCCESS     ${NOCOLOR} ] CORE: proxy containers started\033[0K\r"
-        cd ../..
-        echo
-
-        #
-        # launch:
-        #    db-core
-        #    db-admin
-        #
-        echo -ne "[ ${GREEN}DEPLOYING   ${NOCOLOR} ] CORE: database containers ...\033[0K\r"
-        cd core/database
-
-        # create folders
-        mkdir -p data/logs
-        mkdir -p data/db
-        mkdir -p data/pma/sessions
-
-        echo -ne "[ ${GREEN}CONFIGURING ${NOCOLOR} ] CORE: db core container ...\033[0K\r"
-        printf "\nMYSQL_PASSWORD=${SQL_PASS}\n" >> .env
-        printf "\nNET_NAME=${NET_NAME}\n" >> .env
-        echo -ne "[ ${LIGHTGREEN}SUCCESS     ${NOCOLOR} ] CORE: db core container configured\033[0K\r"
-
-        echo -ne "[ ${GREEN}STARTING    ${NOCOLOR} ] CORE: database containers ...\033[0K\r"
-        docker-compose up -d > /dev/null 2>&1
-        echo -ne "[ ${LIGHTGREEN}SUCCESS     ${NOCOLOR} ] CORE: database containers started\033[0K\r"
-
-        cd ../..
-        echo
-
-        #
-        # launch:
-        #   portainer
-        #
-        cd core/portainer
-        echo -ne "[ ${GREEN}DEPLOYING   ${NOCOLOR} ] CORE: portainer orchestration containers ...\033[0K\r"
-        echo -ne "[ ${GREEN}CONFIGURING ${NOCOLOR} ] CORE: portainer orchestration container ...\033[0K\r"
-        printf "\nPORTAINER_URL=${PORTAINER_URL}\n" >> .env
-        printf "\nPORTAINER_PORT=${PORTAINER_PORT}\n" >> .env
-        printf "\nSSL_MAIL=${SSL_MAIL}\n" >> .env
-        printf "\nNET_NAME=${NET_NAME}\n" >> .env
-        echo -ne "[ ${LIGHTGREEN}SUCCESS     ${NOCOLOR} ] CORE: portainer orchestration container configured\033[0K\r"
-
-        echo -ne "[ ${GREEN}STARTING    ${NOCOLOR} ] CORE: portainer orchestration containers ...\033[0K\r"
-        docker-compose up -d > /dev/null 2>&1
-        echo -ne "[ ${LIGHTGREEN}SUCCESS     ${NOCOLOR} ] CORE: portainer orchestration containers started\033[0K\r"
-
-        cd ../..
-        echo
-
-        # end of deployments
-        echo
-        exit;;
-
-    2)
-        echo
-        echo -ne "[ ${GREEN}STARTING    ${NOCOLOR} ] CORE: all containers ...\033[0K\r"
-        cd core/portainer && docker-compose up -d && cd ../.. > /dev/null 2>&1
-        cd core/database && docker-compose up -d && cd ../.. > /dev/null 2>&1
-        cd core/proxy && docker-compose up -d && cd ../.. > /dev/null 2>&1
-        echo -ne "[ ${LIGHTRED}STARTED     ${NOCOLOR} ] CORE: all containers\033[0K\r"
-        echo
-        exit;;
-
-    3)
-        echo
-        echo -ne "[ ${RED}STOPPING    ${NOCOLOR} ] CORE: all containers ...\033[0K\r"
-        cd core/portainer && docker-compose down && cd ../.. > /dev/null 2>&1
-        cd core/database && docker-compose down && cd ../.. > /dev/null 2>&1
-        cd core/proxy && docker-compose down && cd ../.. > /dev/null 2>&1
-        echo -ne "[ ${LIGHTRED}STOPPED     ${NOCOLOR} ] CORE: all containers\033[0K\r"
-        echo
-        exit;;
-
-    5)
-        echo
-        echo -e "${WHITE}INITIATING DESTRUCT SEQUENCE...${NOCOLOR}"
-        destroy_all
-        exit;;
-    0)
-        echo
-        echo -e "Goodbye!"
-        exit;;
-esac
-
+# end of file
